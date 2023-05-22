9160f7759bd87c955c62d46fd86cfa5183030f3a
jannatul-akhi <jannatulferdous3608@gmail.com>
2023-05-23 00:00:32 +0600

    double-triple-practice

---
+++ js/double.js
@@ -0,0 +1,26 @@
+/*
+1. addEventListener to the button
+2. Get the value of input field
+3. Get the result field
+
+*/
+
+
+// Step-1
+document.getElementById('btn-double').addEventListener('click', function(){
+    // Step-2
+    const inputField = document.getElementById('input-field');
+    const previousInputString = inputField.value;
+    const previousInput = parseFloat(previousInputString);
+
+    if (isNaN(previousInput)) {
+        alert('Please enter a number');
+        return;
+    }
+
+    // Step-3
+    const resultField = document.getElementById('result');
+    const currentResult = previousInput * 2;
+    resultField.innerText = currentResult;
+    
+})
